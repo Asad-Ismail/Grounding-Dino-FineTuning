e24b34ff940ce35e2e36f22df84f2e9398c260c2
Asad-Ismail <asadismaeel@gmail.com>
2023-08-12 15:45:55 +0000

    updated training

--- train.py
+++ train.py
@@ -5,47 +5,107 @@ import json
 import csv
 from collections import defaultdict
 
+# Model
 model = load_model("groundingdino/config/GroundingDINO_SwinT_OGC.py", "weights/groundingdino_swint_ogc.pth")
 
+# Dataset paths
 images_files=sorted(os.listdir("multimodal-data/images"))
 ann_file="multimodal-data/annotation/annotation.csv"
 
-ann_Dict= defaultdict(lambda: defaultdict(list))
 
-with open(ann_file) as file_obj:
-    ann_reader= csv.DictReader(file_obj)  
-    # Iterate over each row in the csv file
-    # using reader object
-    for row in ann_reader:
-        #print(row)
-        img_n=os.path.join("multimodal-data/images",row['image_name'])
-        x1=int(row['bbox_x'])
-        y1=int(row['bbox_y'])
-        x2=x1+int(row['bbox_width'])
-        y2=y1+int(row['bbox_height'])
-        label=row['label_name']
-        ann_Dict[img_n]['boxes'].append([x1,y1,x2,y2])
-        ann_Dict[img_n]['caption'].append(label)
+def draw_box_with_label(image, output_path, coordinates, label, color=(0, 0, 255), thickness=2, font_scale=0.5):
+    """
+    Draw a box and a label on an image using OpenCV.
+
+    Parameters:
+    - image (numpyarray): input image.
+    - output_path (str): Path to save the image with the box and label.
+    - coordinates (tuple): A tuple (x1, y1, x2, y2) indicating the top-left and bottom-right corners of the box.
+    - label (str): The label text to be drawn next to the box.
+    - color (tuple, optional): Color of the box and label in BGR format. Default is red (0, 0, 255).
+    - thickness (int, optional): Thickness of the box's border. Default is 2 pixels.
+    - font_scale (float, optional): Font scale for the label. Default is 0.5.
+    """
+    
+    # Draw the rectangle
+    cv2.rectangle(image, (coordinates[0], coordinates[1]), (coordinates[2], coordinates[3]), color, thickness)
+    
+    # Define a position for the label (just above the top-left corner of the rectangle)
+    label_position = (coordinates[0], coordinates[1]-10)
+    
+    # Draw the label
+    cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness, cv2.LINE_AA)
+    
+    # Save the modified image
+    cv2.imwrite(output_path, image)
+
+def draw_box_with_label(image, output_path, coordinates, label, color=(0, 0, 255), thickness=2, font_scale=0.5):
+    """
+    Draw a box and a label on an image using OpenCV.
+
+    Parameters:
+    - image (str):  Input image.
+    - output_path (str): Path to save the image with the box and label.
+    - coordinates (tuple): A tuple (x1, y1, x2, y2) indicating the top-left and bottom-right corners of the box.
+    - label (str): The label text to be drawn next to the box.
+    - color (tuple, optional): Color of the box and label in BGR format. Default is red (0, 0, 255).
+    - thickness (int, optional): Thickness of the box's border. Default is 2 pixels.
+    - font_scale (float, optional): Font scale for the label. Default is 0.5.
+    """
+    
+    # Draw the rectangle
+    cv2.rectangle(image, (coordinates[0], coordinates[1]), (coordinates[2], coordinates[3]), color, thickness)
+    
+    # Define a position for the label (just above the top-left corner of the rectangle)
+    label_position = (coordinates[0], coordinates[1]-10)
+    
+    # Draw the label
+    cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness, cv2.LINE_AA)
+    
+    # Save the modified image
+    cv2.imwrite(output_path, image)
 
 
-#IMAGE_PATH = "test_pepper.jpg"
-#TEXT_PROMPT = "fruit.stem"
-BOX_TRESHOLD = 0.1
-TEXT_TRESHOLD = 0.2
 
-for IMAGE_PATH,vals in ann_Dict.items():
+def read_dataset(ann_file):
+    ann_Dict= defaultdict(lambda: defaultdict(list))
+    with open(ann_file) as file_obj:
+        ann_reader= csv.DictReader(file_obj)  
+        # Iterate over each row in the csv file
+        # using reader object
+        for row in ann_reader:
+            #print(row)
+            img_n=os.path.join("multimodal-data/images",row['image_name'])
+            x1=int(row['bbox_x'])
+            y1=int(row['bbox_y'])
+            x2=x1+int(row['bbox_width'])
+            y2=y1+int(row['bbox_height'])
+            label=row['label_name']
+            ann_Dict[img_n]['boxes'].append([x1,y1,x2,y2])
+            ann_Dict[img_n]['captions'].append(label)
+    return ann_Dict
 
-    image_source, image = load_image(IMAGE_PATH)
-    print(image_source.shape)
-    print(image.shape)
-    exit()
+def train():
+    ann_Dict=read_dataset(ann_file)
+    for idx, (IMAGE_PATH,vals) in enumerate(ann_Dict.items()):
+        image_source, image = load_image(IMAGE_PATH)
+        # Not ideal use batching from pytorch data loader for multiprocessing but good enough for small dataset
+        for i,bx in enumerate(vals['boxes']):
+            caption=vals['captions'][i]
 
-    boxes, logits, phrases = train(
-        model=model,
-        image_source=image_source,
-        image=image,
-        caption=TEXT_PROMPT,
-        box_target=box_target,
-        box_threshold=BOX_TRESHOLD,
-        text_threshold=TEXT_TRESHOLD
-    )
+            os.makedirs("vis_Dataset",exist_ok=True)
+            draw_box_with_label(image_source,f"vis_Dataset/{idx}.png" ,bx,caption)
+            continue
+
+            boxes, logits, phrases = train(
+                model=model,
+                image_source=image_source,
+                image=image,
+                caption=TEXT_PROMPT,
+                box_target=box_target,
+                box_threshold=BOX_TRESHOLD,
+                text_threshold=TEXT_TRESHOLD
+            )
+
+if __name__=="__main__":
+    train()
